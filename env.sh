aea56e93bd818875357ab83e28e9af6a516f615a
Hidetake Iwata <int128@gmail.com>
2018-04-26 18:13:33 +0900

    Moving to template

---
+++ env.sh
@@ -0,0 +1,5 @@
+# Base domain
+DEVOPS_DOMAIN="devops.example.com"
+
+# PostgreSQL host
+DEVOPS_POSTGRES_HOST="xxx.xxx.us-west-2.rds.amazonaws.com"
---
+++ render.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+set -e
+set -o pipefail
+set -x
+
+cd "$(dirname "$0")"
+source env.sh
+
+
